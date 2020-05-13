e6e02397d2e6b3aaaf56eb1d5dc1a5ea793a149a
Roddy <roddytdog@yahoo.com>
2020-05-13 16:28:07 -0700

    missing updates

--- src/components/HiddenComponent/HiddenCompent.tsx
+++ src/components/HiddenComponent/HiddenCompent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HBD from '../../images/otters_HBD.jpg';
+import MC from '../../images/MC_HBD.jpg';
 
 export type HiddenComponentProps = {
     agent: string;
@@ -9,19 +10,33 @@ export type HiddenComponentProps = {
 const HiddenComponent = (props: HiddenComponentProps) => {
 
     let salutationSignatures: string = "";
+    let imageUrl: string = "";
+    let ff: string = "";
+    let bdayMsg: string = "";
+
+    if (props.agent.toLocaleLowerCase() === "buddy") {
+        imageUrl = HBD
+        ff = 'Bradley-Hand, cursive'
+        bdayMsg = props.agent.toUpperCase() + ', the secret to success for animals is to enjoy every day like its your birthday.';
+    } else {
+        imageUrl = MC
+        ff = 'fantasy'
+        bdayMsg = props.agent.toUpperCase() + ', the mission was a breeze. Now enjoying your birthday is the next mission. ARE YOU READY?'
+    }
+
     props.missionControl.map((mc, index) => salutationSignatures += `${mc} `);
 
     return (
         <div 
-        style={{backgroundImage: `url(${HBD})`,
+        style={{backgroundImage: `url(${imageUrl})`,
                 backgroundSize: 'cover',
                 height: '100%',
-                fontFamily: 'Bradley-Hand, cursive',
+                fontFamily: ff,
                 fontSize: '20px'}} 
         className={'showme'}
         >
             <h2 style={{margin: 0, wordWrap: 'normal'}}>
-                {props.agent}, The secret to success for animals is to enjoy every day like its your birthday.
+                {bdayMsg}
             </h2>
             <h3 style={{width: '100%', textAlign: 'center'}}> From: {salutationSignatures}</h3>
         </div>
